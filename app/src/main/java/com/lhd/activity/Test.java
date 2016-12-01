bf7f1d812d21815cb82a37d82a0d855e3201a3f5
LeHongDuong <24duong@gmail.com>
2016-12-01 22:39:59 +0700

    clone lai

--- app/src/main/java/com/lhd/activity/Test.java
+++ app/src/main/java/com/lhd/activity/Test.java
@@ -15,7 +15,7 @@ public class Test extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-
+//sdasd
 
     }
 
